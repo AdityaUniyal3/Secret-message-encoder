17b842df77b5d239d9aa4d370db2f5a98c8b0c24
AdityaUniyal3 <118508851+AdityaUniyal3@users.noreply.github.com>
2023-03-29 22:26:16 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,28 @@
+//decoding hash
+
+const{hash} = window.location;
+
+//remove symbols before function decodes 
+const message = atob(hash.replace('#',''));
+
+if(message){
+	document.querySelector('#message-form').classList.add('hide');
+	document.querySelector('#message-show').classList.remove('hide');
+
+	document.querySelector('h1').innerHTML=message;
+}
+
+
+document.querySelector('form').addEventListener('submit',event=>{event.preventDefault();
+
+	//toggle visibility by removing hide class
+	document.querySelector('#message-form').classList.add('hide');
+	document.querySelector('#link-form').classList.remove('hide');
+
+	const input = document.querySelector('#message-input');
+	const encrypted = btoa(input.value);
+
+	const linkInput = document.querySelector('#link-input')
+	linkInput.value = `${window.location}#${encrypted}`;
+	linkInput.select();
+});
